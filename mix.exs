ee814a39a74c6d64304112bc7b560df0477f71b9
Aryan Shrestha <aryanstha4859@gmail.com>
2023-12-03 22:28:22 +0545

    Create mix.exs

---
+++ mix.exs
@@ -0,0 +1,28 @@
+# mix.exs
+
+defmodule MyApp.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :my_app,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:plug, "~> 1.12"},
+      {:mongodb, "~> 0.7"}
+    ]
+  end
+end
