489a3b0db2788410ab7eceffff7767561da79d1e
Aryan Shrestha <aryanstha4859@gmail.com>
2023-12-04 19:40:15 +0545

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,5 +1,3 @@
-// fetch.js
-
 // Fetch data from the API
 fetch('https://leader4.onrender.com/api/users')
   .then(response => response.json())
@@ -15,29 +13,34 @@ fetch('https://leader4.onrender.com/api/users')
   })
   .catch(error => console.error('Error fetching data:', error));
 
-// Function to display the leaderboard in a table
+// Function to display the leaderboard in a table with Bootstrap styles
 function displayLeaderboard(data) {
   const leaderboardElement = document.getElementById('leaderboard');
 
-  // Create a table element
+  // Create a table element with Bootstrap classes
   const table = document.createElement('table');
-  table.classList.add('table', 'table-bordered');
+  table.classList.add('table', 'table-striped', 'table-hover');
 
-  // Create the table header
+  // Create the table header with Bootstrap classes
   const thead = document.createElement('thead');
+  thead.classList.add('thead-dark');
   const headerRow = document.createElement('tr');
+
   const rankHeader = document.createElement('th');
   rankHeader.scope = 'col';
   rankHeader.textContent = 'Rank';
   headerRow.appendChild(rankHeader);
+
   const nameHeader = document.createElement('th');
   nameHeader.scope = 'col';
   nameHeader.textContent = 'Name';
   headerRow.appendChild(nameHeader);
+
   const impressionsHeader = document.createElement('th');
   impressionsHeader.scope = 'col';
   impressionsHeader.textContent = 'Impressions';
   headerRow.appendChild(impressionsHeader);
+
   thead.appendChild(headerRow);
   table.appendChild(thead);
 
@@ -45,7 +48,7 @@ function displayLeaderboard(data) {
   const tbody = document.createElement('tbody');
   data.forEach((user, index) => {
     const row = document.createElement('tr');
-    
+
     const rankCell = document.createElement('td');
     rankCell.textContent = index + 1;
     row.appendChild(rankCell);
